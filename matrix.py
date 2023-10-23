368aa3a2096e0a7817a85b4364cc17d09fd306d5
jera <aloyobrendaojera@gmail.com>
2023-10-23 11:32:00 +0300

    MATRIX WORK

---
+++ matrix.py
@@ -0,0 +1,15 @@
+#MATRIX MANIPULATION WITH NUMPY.
+#MATRIX ADDITION.
+"""x+y-2z=7
+2x-y-4z=2
+-5x+4y+10z=1
+3x-y-6z=5"""
+
+#First, you need to represent the system of equations in matrix form. Let's call the matrix of coefficients A, the vector of variables X, and the vector of constants B:
+A = [[1, 1, -2], [2, -1, -4], [-5, 4, 10], [3, -1, -6]]
+
+X = [['x'], ['y'], ['z']]
+
+B = [[7], [2], [1], [5]]
+
+
